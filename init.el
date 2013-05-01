d78b9f2857cb42b3255fa719482cc6db7ea97cc5
bcasiello <brian.casiello@autonomy.com>
2013-05-01 13:54:57 -0400

    Pallet-based installation

--- init.el
+++ init.el
@@ -1,17 +1,6 @@
-;; Load up Org Mode and Org Babel for elisp embedded in Org Mode files
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-(if (string= system-name "weatherwax")
-    (setq dotfiles-dir "/home/brian/.emacs.d"))
-(defun add-subdirs-to-load-path (dir)
-  "Recursive add directories to `load-path'."
-  (let ((default-directory (file-name-as-directory dir)))
-    (add-to-list 'load-path dir);
-	(message "added %s" dir)
-    (normal-top-level-add-subdirs-to-load-path)))
-(add-subdirs-to-load-path (expand-file-name "site-lisp" dotfiles-dir))
-(add-subdirs-to-load-path (expand-file-name "elpa" dotfiles-dir))
+;; initialize packages
+(add-to-list 'load-path (car (file-expand-wildcards "~/.emacs.d/elpa/pallet*")))
+(require 'pallet)
 
-;; tangle and load the startup file
-(require 'org-install)
-(org-babel-do-load-languages 'org-babel-load-languages '((lua . t)))
-(org-babel-load-file (expand-file-name "startup.org" dotfiles-dir))
+;; Tangle and load the literate startup file
+(org-babel-load-file "~/.emacs.d/startup.org")
